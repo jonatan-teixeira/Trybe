d4f422a9c68a3464b2bae03fb282e1c54bd23273
jonatan-teixeira <jonatan.teixeira@live.com>
2021-07-28 19:44:37 -0300

    Bloco 4 - Aula 1

---
+++ Bloco-4-Aula-1/4.js
@@ -0,0 +1,11 @@
+//4
+let num = -50;
+
+if (num > 0) {
+    console.log('POSITIVO')
+} else if (num <0) {
+        console.log('NEGATIVO')
+    }else if (num ===0) {
+            console.log('ZERO')
+        };
+    
