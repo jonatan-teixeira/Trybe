37093a710338f8e369e60d40648de2c3f2864ce2
jonatan-teixeira <jonatan.teixeira@live.com>
2021-08-02 16:56:46 -0300

    Atualizacao do diretorio

---
+++ Bloco4/Dia3/1.js
@@ -0,0 +1,25 @@
+//1
+let n = 5;
+for (let i = 0; i < n; i+=1){
+    let str = "*"
+    console.log('1.'+str.repeat(n));
+}  
+
+//2
+let nn = 5;
+
+for (let i = 1; i <= nn; i+=1){
+  asterisco = ""
+  for (let linha = 1; linha < nn; linha+=1){
+    asterisco += i
+  }
+  console.log('2.'+asterisco)
+}
+
+
+//3
+//let str = "";
+//for(count = 0; count < 5; count++){
+    //for(let i = 5; i > count; i--)
+//    console.log(str);
+//}
