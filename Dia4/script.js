ed6d3d1f19e6d99e225ba66b6fd5375d8bc95a59
jonatan-teixeira <jonatan.teixeira@live.com>
2021-08-02 16:55:01 -0300

    Dia4 sendo feito

---
+++ Dia4/script.js
@@ -0,0 +1,30 @@
+// let name = 'Marta';
+// let lastName = 'Silva';
+// let age = 34;
+// let medals = { golden: 2, silver: 3 };
+// let bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
+
+// console.log('A jogadora ' + name + ' ' + lastName + ' foi eleita a melhor do mundo por ' + bestInTheWorld.length +' vezes')
+// console.log('A jogadora possui ' + medals.golden + ' medalhas de ouro e ' + medals.silver + ' medalhas de prata.')
+
+// let names = {
+//     person1: 'João',
+//     person2: 'Maria',
+//     person3: 'Jorge'
+// };
+
+// for (let key in names){
+//     console.log('Olá '+ names[key])
+// }
+
+// let car = {
+//     model: 'A3 Sedan',
+//     manufacturer: 'Audi',
+//     year: 2020
+// }
+
+// for (let key in car){
+//     console.log(key, car[key])
+// }
+
+
